6776749c07d2bb785db21c6cdf8e7cf21c53f3ed
Niccolò Brogi <me@nbrogi.com>
2017-03-14 00:27:10 +0100

    Improve PHP class

--- php/src/nkkollaw/Multitenancy/Validators/Subdomain.php
+++ php/src/nkkollaw/Multitenancy/Validators/Subdomain.php
@@ -1,28 +1,49 @@
 <?php
 namespace nkkollaw\Multitenancy\Validators;
-
 class Subdomain
 {
-    public static function isReserved($subdomain) {
-        $yaml = file_get_contents(__DIR__ . '/../../../../../reserved-subdomains.yaml');
+
+    public static function readYAML($path)
+	{
+        $yaml = file_get_contents($path);
         if (!$yaml) {
-            throw new \Exception('unable to find YAML with subdomains');
+            throw new \Exception('unable to find YAML file');
         }
 
         $reserved_subdomains = \Symfony\Component\Yaml\Yaml::parse($yaml);
+		if (!$reserved_subdomains) {
+			throw new \Exception('unable to parse YAML');
+		}
 
+		return $reserved_subdomains;
+    }
+
+    public static function isRegex($str)
+    {
+        return strpos($str, '/') !== false;
+    }
+
+    public static function isReserved($subdomain, $path = __DIR__ . '/../../../../../reserved-subdomains.yaml')
+    {
+        $reserved_subdomains = self::readYAML($path);
+
+        $is_reserved = false;
         foreach ($reserved_subdomains as $reserved_subdomain) {
-            if (strpos($reserved_subdomain, '/') !== false) { // is regex ?
+            if (self::isRegex($reserved_subdomain)) {
                 if (preg_match($reserved_subdomain, $subdomain)) {
-                    return true;
+                    $is_reserved = true;
+
+                    break;
                 }
             } else {
-                if (strpos($reserved_subdomain, $subdomain) !== false) {
-                    return true;
+                if ($reserved_subdomain == $subdomain) {
+                    $is_reserved = true;
+
+                    break;
                 }
             }
         }
 
-        return false;
+        return $is_reserved;
     }
 }
